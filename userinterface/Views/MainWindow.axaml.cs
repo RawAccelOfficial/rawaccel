3c40a63e74c230a41db746cd925806a28f9340c5
Lex <lex.k.moulton@gmail.com>
2025-06-21 23:08:18 -0400

    Add theme toggle

--- userinterface/Views/MainWindow.axaml.cs
+++ userinterface/Views/MainWindow.axaml.cs
@@ -1,5 +1,8 @@
+﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
 using userinterface.ViewModels;
 
 namespace userinterface.Views;
@@ -9,6 +12,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        UpdateThemeToggleButton();
     }
 
     public void ApplyButtonHandler(object sender, RoutedEventArgs args)
@@ -16,6 +20,41 @@ public partial class MainWindow : Window
         if (this.DataContext is MainWindowViewModel viewModel)
         {
             viewModel.ApplyButtonClicked();
+        }
+    }
+
+    private void ToggleTheme(object sender, RoutedEventArgs e)
+    {
+        var currentTheme = Application.Current?.ActualThemeVariant;
+        var newTheme = currentTheme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
+
+        if (Application.Current != null)
+        {
+            Application.Current.RequestedThemeVariant = newTheme;
+        }
+
+        UpdateThemeToggleButton();
+    }
+
+    private void UpdateThemeToggleButton()
+    {
+        var themeIcon = this.FindControl<PathIcon>("ThemeIcon");
+        var toggleButton = this.FindControl<ToggleButton>("ThemeToggleButton");
+
+        if (themeIcon != null && toggleButton != null)
+        {
+            var isDark = Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
+
+            if (isDark)
+            {
+                themeIcon.Data = (Avalonia.Media.Geometry?)this.FindResource("weather_moon_regular");
+            }
+            else
+            {
+                themeIcon.Data = (Avalonia.Media.Geometry?)this.FindResource("weather_sunny_regular");
+            }
+
+            toggleButton.IsChecked = !isDark;
         }
     }
 }
