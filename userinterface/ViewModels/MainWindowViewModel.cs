f791422095f3994f6d5aa07a848fa9a18b9b0b36
Lex <lex.k.moulton@gmail.com>
2025-06-25 03:21:55 -0400

    Pretty up the nav menu

--- userinterface/ViewModels/MainWindowViewModel.cs
+++ userinterface/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using userinterface.ViewModels.Device;
 using userinterface.ViewModels.Mapping;
 using userinterface.ViewModels.Profile;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using BE = userspace_backend;
 
 namespace userinterface.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public partial class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 {
+    private string _selectedPage = "Devices";
+
     public MainWindowViewModel(BE.BackEnd backEnd)
     {
         BackEnd = backEnd;
@@ -16,15 +20,52 @@ public partial class MainWindowViewModel : ViewModelBase
     }
 
     public DevicesPageViewModel DevicesPage { get; }
-
     public ProfilesPageViewModel ProfilesPage { get; }
-
     public MappingsPageViewModel MappingsPage { get; }
-
     protected BE.BackEnd BackEnd { get; set; }
+
+    public string SelectedPage
+    {
+        get => _selectedPage;
+        set
+        {
+            if (_selectedPage != value)
+            {
+                _selectedPage = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(CurrentPageContent));
+            }
+        }
+    }
+
+    public object? CurrentPageContent
+    {
+        get
+        {
+            return SelectedPage switch
+            {
+                "Devices" => DevicesPage,
+                "Mappings" => MappingsPage,
+                "Profiles" => ProfilesPage,
+                _ => DevicesPage
+            };
+        }
+    }
+
+    public void SelectPage(string pageName)
+    {
+        SelectedPage = pageName;
+    }
 
     public void ApplyButtonClicked()
     {
         BackEnd.Apply();
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- userinterface/Views/MainWindow.axaml.cs
+++ userinterface/Views/MainWindow.axaml.cs
@@ -13,6 +13,7 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         UpdateThemeToggleButton();
+        UpdateSelectedButton("Devices"); // Set initial navigation selection
     }
 
     public void ApplyButtonHandler(object sender, RoutedEventArgs args)
@@ -23,16 +24,48 @@ public partial class MainWindow : Window
         }
     }
 
+    public void OnNavigationClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is string pageName)
+        {
+            if (DataContext is MainWindowViewModel viewModel)
+            {
+                viewModel.SelectPage(pageName);
+                UpdateSelectedButton(pageName);
+            }
+        }
+    }
+
+    private void UpdateSelectedButton(string selectedPage)
+    {
+        // Remove Selected class from all navigation buttons
+        DevicesButton.Classes.Remove("Selected");
+        MappingsButton.Classes.Remove("Selected");
+        ProfilesButton.Classes.Remove("Selected");
+
+        // Add Selected class to the current button
+        switch (selectedPage)
+        {
+            case "Devices":
+                DevicesButton.Classes.Add("Selected");
+                break;
+            case "Mappings":
+                MappingsButton.Classes.Add("Selected");
+                break;
+            case "Profiles":
+                ProfilesButton.Classes.Add("Selected");
+                break;
+        }
+    }
+
     private void ToggleTheme(object sender, RoutedEventArgs e)
     {
         var currentTheme = Application.Current?.ActualThemeVariant;
         var newTheme = currentTheme == ThemeVariant.Dark ? ThemeVariant.Light : ThemeVariant.Dark;
-
         if (Application.Current != null)
         {
             Application.Current.RequestedThemeVariant = newTheme;
         }
-
         UpdateThemeToggleButton();
     }
 
@@ -40,11 +73,9 @@ public partial class MainWindow : Window
     {
         var themeIcon = this.FindControl<PathIcon>("ThemeIcon");
         var toggleButton = this.FindControl<ToggleButton>("ThemeToggleButton");
-
         if (themeIcon != null && toggleButton != null)
         {
             var isDark = Application.Current?.ActualThemeVariant == ThemeVariant.Dark;
-
             if (isDark)
             {
                 themeIcon.Data = (Avalonia.Media.Geometry?)this.FindResource("weather_moon_regular");
@@ -53,7 +84,6 @@ public partial class MainWindow : Window
             {
                 themeIcon.Data = (Avalonia.Media.Geometry?)this.FindResource("weather_sunny_regular");
             }
-
             toggleButton.IsChecked = !isDark;
         }
     }
