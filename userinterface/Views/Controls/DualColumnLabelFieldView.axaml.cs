ff9de8d7c7e07093e370199b4560238dd6b05b33
Lex <lex.k.moulton@gmail.com>
2025-07-19 20:57:40 -0400

    Expander anamation

--- userinterface/Views/Controls/DualColumnLabelFieldView.axaml.cs
+++ userinterface/Views/Controls/DualColumnLabelFieldView.axaml.cs
@@ -1,47 +1,19 @@
-﻿using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
+using Avalonia.Controls;
 using userinterface.ViewModels.Controls;
 
 namespace userinterface.Views.Controls;
 
 public partial class DualColumnLabelFieldView : UserControl
 {
-    private StackPanel? StackPanel;
-
-    private StackPanel? StackPanelInstance
+    public DualColumnLabelFieldView()
     {
-        get => StackPanel;
-        set
-        {
-            StackPanel = value;
-            if (StackPanel != null && ViewModel != null)
-            {
-                ViewModel.SetStackPanelCommand.Execute(StackPanel);
-            }
-        }
-    }
-
-    public DualColumnLabelFieldViewModel? ViewModel => DataContext as DualColumnLabelFieldViewModel;
-
-    public DualColumnLabelFieldView() : this(new DualColumnLabelFieldViewModel())
-    {
+        InitializeComponent();
+        DataContext = new DualColumnLabelFieldViewModel();
     }
 
     public DualColumnLabelFieldView(DualColumnLabelFieldViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
-        InitializeStackPanel();
-    }
-
-    private void InitializeStackPanel()
-    {
-        StackPanelInstance = this.FindControl<StackPanel>("MainStackPanel");
-    }
-
-    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-    {
-        base.OnApplyTemplate(e);
-        StackPanelInstance = this.FindControl<StackPanel>("MainStackPanel");
     }
 }
