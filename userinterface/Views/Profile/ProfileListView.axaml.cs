f237e86637f79ef6adbd86615cb22c1434652b13
Lex <lex.k.moulton@gmail.com>
2025-07-22 19:40:54 -0400

    Get rid of element

--- userinterface/Views/Profile/ProfileListView.axaml.cs
+++ userinterface/Views/Profile/ProfileListView.axaml.cs
@@ -1,15 +1,29 @@
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 using userinterface.ViewModels.Profile;
+using System;
 using System.ComponentModel;
+using Avalonia.Styling;
 
 namespace userinterface.Views.Profile;
 
 public partial class ProfileListView : UserControl
 {
+    private Animation rectangleAnimation;
+    private Rectangle animatedRectangle;
+
     public ProfileListView()
     {
         InitializeComponent();
         DataContextChanged += OnDataContextChanged;
+        Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        animatedRectangle = this.FindControl<Rectangle>("AnimatedRectangle");
     }
 
     private void OnDataContextChanged(object sender, System.EventArgs e)
@@ -25,8 +39,44 @@ public partial class ProfileListView : UserControl
         if (e.PropertyName == nameof(ProfileListViewModel.CurrentPosition))
         {
             var viewModel = (ProfileListViewModel)sender;
-            var profileElement = this.FindControl<ProfileListElementView>("ProfileElement");
-            profileElement?.AnimateToPosition(viewModel.CurrentPosition);
+            AnimateRectangleToPosition(viewModel.CurrentPosition);
         }
+    }
+
+    private void AnimateRectangleToPosition(int position)
+    {
+        if (animatedRectangle?.RenderTransform is not TranslateTransform transform) return;
+
+        double targetY = position * 50d; // position 0 = 0px, position 1 = 50px
+
+        rectangleAnimation = new Animation
+        {
+            Duration = TimeSpan.FromSeconds(1),
+            FillMode = FillMode.Forward, // Keep final state
+            Children =
+            {
+                new KeyFrame
+                {
+                    Cue = new Cue(1d),
+                    Setters =
+                    {
+                        new Setter
+                        {
+                            Property = TranslateTransform.YProperty,
+                            Value = targetY
+                        }
+                    }
+                }
+            }
+        };
+
+        // Also set the transform directly after animation completes
+        rectangleAnimation.RunAsync(animatedRectangle).ContinueWith(_ => 
+        {
+            Avalonia.Threading.Dispatcher.UIThread.Post(() => 
+            {
+                transform.Y = targetY;
+            });
+        });
     }
 }
