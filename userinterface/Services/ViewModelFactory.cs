2fb86de1b5346869009a3184735e410b210d32e9
Lex <lex.k.moulton@gmail.com>
2025-07-25 16:40:19 -0400

    Remove JIT logging

--- userinterface/Services/ViewModelFactory.cs
+++ userinterface/Services/ViewModelFactory.cs
@@ -21,15 +21,8 @@ namespace userinterface.Services
 
         public ProfileViewModel CreateProfileViewModel(BE.ProfileModel profileModel)
         {
-            var sw = Stopwatch.StartNew();
             var viewModel = ServiceProvider.GetRequiredService<ProfileViewModel>();
-            sw.Stop();
-            Debug.WriteLine($"[JIT-PERF] ProfileViewModel creation: {sw.ElapsedMilliseconds}ms");
-            
-            sw.Restart();
             viewModel.Initialize(profileModel);
-            sw.Stop();
-            Debug.WriteLine($"[JIT-PERF] ProfileViewModel.Initialize: {sw.ElapsedMilliseconds}ms");
             return viewModel;
         }
 
